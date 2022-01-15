414b279c69a1d3f3991699715fe97101452fa3e9
Kauê Jacyntho <kauekaj@gmail.com>
2022-01-15 15:38:50 -0300

    Adicionando todos os metodos http e finalizando a aplicacao

---
+++ src/routes/users.route.ts
@@ -0,0 +1,33 @@
+import { Router, Response, Request, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+const userRoute = Router();
+
+userRoute.get('/users', (req: Request , res: Response , next: NextFunction ) => {
+    const users = [{ userName: 'Renan' }];
+    res.status(StatusCodes.OK).send(users);
+});
+
+userRoute.get('/users/:uuid', (req: Request<{ uuid: string }> , res: Response , next: NextFunction ) => {
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({ uuid });
+});
+
+userRoute.post('/users', (req: Request , res: Response , next: NextFunction ) => {
+    const newUser = req.body;
+    res.status(StatusCodes.CREATED).send(newUser);
+});
+
+userRoute.put('/users/:uuid', (req: Request<{ uuid: string }> , res: Response , next: NextFunction ) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+    modifiedUser.uuid = uuid;
+    res.status(StatusCodes.OK).send(modifiedUser);
+});
+
+userRoute.delete('/users/:uuid', (req: Request<{ uuid: string }> , res: Response , next: NextFunction ) => {
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK);
+});
+
+export default userRoute;
