301964aba843b5d86cd8036c0def2507a6436871
Kauê Jacyntho <kauekaj@gmail.com>
2022-01-15 15:37:31 -0300

    atualizando index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from "express";
+import statusRoute from "./routes/status.route";
+import userRoute from "./routes/users.route";
 
 const app = express();
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Sucesso Total!' });
-});
+//Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Configurações de Rotas
+app.use(statusRoute);
+app.use(userRoute);
 
+//Inicialização do servidor
 app.listen(3000, () => {
-    console.log('Aplicação executando na porta 3000');
-})
+  console.log("Aplicação executando na porta 3000");
+});
