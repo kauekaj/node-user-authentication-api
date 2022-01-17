1dde5b523f2ed02e2fca4c80837ac7af1ff466d8
Kauê Jacyntho <kauekaj@gmail.com>
2022-01-17 20:24:44 -0300

    Adição do middleware na authentication

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,39 +1,18 @@
 import { NextFunction, Request, Response, Router } from 'express';
-import ForbiddenError from '../models/errors/forbidden.error.model';
-import userRepository from '../repositories/user.repository';
 import JWT from 'jsonwebtoken'
 import { StatusCodes } from 'http-status-codes';
+import basicAuthenticationMiddleware from '../middlewares/basic-authentication.middleware';
+import ForbiddenError from '../models/errors/forbidden.error.model';
 
 
 const authorizationRoute = Router();
 
-authorizationRoute.post('/token', async (req: Request, res: Response, next: NextFunction) => {
-    try {       
-        const authorizationHeader = req.headers['authorization'];
-        
-        if (!authorizationHeader) {
-            throw new ForbiddenError('Credenciais não informadas');
-        }
-        
-        const [authenticationType, token] = authorizationHeader.split(' ');
+authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Request, res: Response, next: NextFunction) => {
+    try {      
+        const user = req.user; 
 
-        if (authenticationType !== 'Basic' || !token) {
-            throw new ForbiddenError('Tipo de autenticação inválido');
-        }
-
-        const tokenContent = Buffer.from(token, 'base64').toString('utf-8')
-
-        const [username, password] = tokenContent.split(':');
-        
-        if (!username || !password) {
-            throw new ForbiddenError('Credenciais não preenchidas');
-        }
-
-        const user = await userRepository.findByUsernameAndPassword(username, password);
-        console.log(user);
-            
         if (!user) {
-            throw new ForbiddenError('Usuário ou senha inválidos');
+            throw new ForbiddenError('Usuário não informado!');
         }
 
         const jwtPayload = { username: user.username };
@@ -41,7 +20,7 @@ authorizationRoute.post('/token', async (req: Request, res: Response, next: Next
         const secretKey = 'my_secret_key';
 
         const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions);
-        
+
         res.status(StatusCodes.OK).json({ token: jwt });
 
     } catch (error) {
